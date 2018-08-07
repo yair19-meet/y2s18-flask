68fd3a283df575b47c57b3764cca04f6a9daa141
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 16:40:11 +0300

    finished new labs

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    return render_template("index.html", players = ["Derrick Rose", "Stephen curry", "Lebron James", "Kevin Durant"], likes_same_sport=False)
 
 if __name__ == '__main__':
    app.run(debug = True)
